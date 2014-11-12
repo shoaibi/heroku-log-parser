905c849a288ce0c0a0505aead3ffe5c97c5d22c0
Muhammad Shoaib <shoaib@bitesource.com>
2014-11-12 13:46:41 +0500

    Updated wrapper.sh to depict how same script can be used to gather data for non-user-id urls too

--- src/wrapper.sh
+++ src/wrapper.sh
@@ -10,6 +10,7 @@ params[2]="GET /api/users/{user_id}/get_friends_progress"
 params[3]="GET /api/users/{user_id}/get_friends_score"
 params[4]="POST /api/users/{user_id}"
 params[5]="GET /api/users/{user_id}"
+params[6]="POST /version_api/files"
 
 for param in "${params[@]}"
 do
